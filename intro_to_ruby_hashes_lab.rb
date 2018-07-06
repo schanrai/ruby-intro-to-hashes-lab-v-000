250eb7dea74abef7c5fbf7f1fcae6747eb3803e1
Sushi Chanrai <sushi@pathworks.co>
2018-07-06 20:16:18 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,9 +35,7 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces => 4,
 			:names => {
-				:reading_railroad => {
-					"mortgage_value" => "$100"
-				}
+				:reading_railroad => {"mortgage_value" => "$100"},
 				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline => {}
