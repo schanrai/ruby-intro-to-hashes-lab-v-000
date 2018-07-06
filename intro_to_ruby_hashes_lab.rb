d916c845cc141581e65b81daad2862ff101265f5
Sushi Chanrai <sushi@pathworks.co>
2018-07-06 19:48:03 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,11 +23,8 @@ def monopoly_with_second_tier
 	monopoly = {
 		:railroads => {
 			:pieces => 4,
-			:rent_in_dollars => {}
-			{
-				:one_piece_owned => 25,
-			},
-			:names => {}
+			:names => {},
+			:rent_in_dollars => { :one_piece_owned => 25}
 			}
 		}
 end
